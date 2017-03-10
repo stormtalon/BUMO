a9404101e4f0c6396452805c14569ec9e33b48fd
cavdarmerve <mervecavdar@sabanciuniv.edu>
2017-03-10 11:51:29 +0300

    Add events to the side panel buttons

--- BUMO/week-3-2/src/chat/side-panel/controller.js
+++ BUMO/week-3-2/src/chat/side-panel/controller.js
@@ -1,6 +1,19 @@
 module.exports = class SidePanelController {
     constructor() {
-        
     }
 }
 
+module.exports = class SidePanelController {
+    loginBtn() {
+        location.href = "https://accounts.spotify.com/tr-TR/login?continue=https:%2F%2Fwww.spotify.com%2Ftr%2Faccount%2Foverview%2F";
+    }
+    combGenresBtn() {
+        alert("Combine genres")
+    }
+    saveListBtn() {
+        alert("Save List")
+    }
+    aboutBtn() {
+        document.write("We are hard-working IT students Merve, Onur, Berk and Ulas, studying in Sabanci University IT Master Programme and this is our project for our Front-end development class. We hope that you will enjoy using this application as much as we had fun developing it!");
+    }
+}
